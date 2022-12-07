3764d6161380dd0fb99069ff487ae65800f6c51a
Bence Rozsa <robence@gmail.com>
2022-12-07 09:37:45 +0100

    feat(graph): query a subgraph

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { createClient } from "urql";
+import { useEffect } from "react";
+
+const apiKey = process.env.REACT_APP_API_KEY;
+const URL = `https://gateway.thegraph.com/api/${apiKey}/subgraphs/id/FDD65maya4xVfPnCjSgDRBz6UBWKAcmGtgY6BmUueJCg`;
+
+const query = `
+{
+  protocols(first: 5) {
+    id
+    inflation
+    inflationChange
+    maxEarningsClaimsRounds
+  }
+  transcoders(first: 5) {
+    id
+    activationRound
+    deactivationRound
+    lastActiveStakeUpdateRound
+  }
+}
+`;
+
+const client = createClient({ url: URL });
 
 function App() {
+  async function fetchData() {
+    const response = await client.query(query).toPromise();
+    console.log(response);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
